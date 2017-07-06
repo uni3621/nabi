29419459cccb29c457e2c951a171c28a4a52cd4c
user-PC\user <uni3621@naver.com>
2017-07-06 19:17:59 +0900

    no message

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.myapplication;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabHost tabHost = (TabHost)findViewById(R.id.tabHost1);
+        tabHost.setup();
+
+        TabHost.TabSpec homeTab = tabHost.newTabSpec("Home Tab");
+        homeTab.setContent(R.id.home_tab);
+        homeTab.setIndicator("", getResources().getDrawable(R.drawable.home_icon));
+        tabHost.addTab(homeTab);
+
+        TabHost.TabSpec trafficTab = tabHost.newTabSpec("Traffic tab");
+        trafficTab.setContent(R.id.traffic_tab);
+        trafficTab.setIndicator("TRAFFIC");
+        tabHost.addTab(trafficTab);
+
+    }
+}
