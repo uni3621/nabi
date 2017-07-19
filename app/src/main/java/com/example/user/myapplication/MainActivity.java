bbbe0832ffa02207362b39b2e39a01fc1463577c
user-PC\user <uni3621@naver.com>
2017-07-19 15:11:52 +0900

    spending파일 잘못만듬

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -13,33 +13,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TabHost tabHost = (TabHost)findViewById(R.id.tabHost1);
-        tabHost.setup();
+        TabHost maintabHost = (TabHost)findViewById(R.id.mainTabHost);
+        maintabHost.setup();
 
-        TabHost.TabSpec homeTab = tabHost.newTabSpec("Home Tab");
+        TabHost.TabSpec homeTab = maintabHost.newTabSpec("Home Tab");
         homeTab.setContent(R.id.home_tab);
         homeTab.setIndicator("", getResources().getDrawable(R.drawable.home_icon));
-        tabHost.addTab(homeTab);
+        maintabHost.addTab(homeTab);
 
-        TabHost.TabSpec trafficTab = tabHost.newTabSpec("Traffic Tab");
+        TabHost.TabSpec trafficTab = maintabHost.newTabSpec("Traffic Tab");
         trafficTab.setContent(R.id.traffic_tab);
         trafficTab.setIndicator("", getResources().getDrawable(R.drawable.traffic_icon));
-        tabHost.addTab(trafficTab);
+        maintabHost.addTab(trafficTab);
 
-        TabHost.TabSpec weatherTab = tabHost.newTabSpec("Weather Tab");
+        TabHost.TabSpec weatherTab = maintabHost.newTabSpec("Weather Tab");
         weatherTab.setContent(R.id.weather_tab);
         weatherTab.setIndicator("", getResources().getDrawable(R.drawable.weather_icon));
-        tabHost.addTab(weatherTab);
+        maintabHost.addTab(weatherTab);
 
-        TabHost.TabSpec spendingTab = tabHost.newTabSpec("Spending Tab");
+        TabHost.TabSpec spendingTab = maintabHost.newTabSpec("Spending Tab");
         spendingTab.setContent(R.id.spending_tab);
         spendingTab.setIndicator("", getResources().getDrawable(R.drawable.spending_icon));
-        tabHost.addTab(spendingTab);
+        maintabHost.addTab(spendingTab);
 
-        TabHost.TabSpec mypageTab = tabHost.newTabSpec("Mypage Tab");
+        TabHost.TabSpec mypageTab = maintabHost.newTabSpec("Mypage Tab");
         mypageTab.setContent(R.id.mypage_tab);
         mypageTab.setIndicator("", getResources().getDrawable(R.drawable.mypage_icon));
-        tabHost.addTab(mypageTab);
+        maintabHost.addTab(mypageTab);
+
+        TabHost spendingtabHost = (TabHost)findViewById(R.id.spendingtabHost);
+        spendingtabHost.setup();
+
+        TabHost.TabSpec monthtab = spendingtabHost.newTabSpec("Month Tab");
+        monthtab.setContent(R.id.month_Tab);
+        monthtab.setIndicator("월별");
+        spendingtabHost.addTab(monthtab);
+
+        TabHost.TabSpec weektab = spendingtabHost.newTabSpec("Week Tab");
+        weektab.setContent(R.id.week_Tab);
+        weektab.setIndicator("주별");
+        spendingtabHost.addTab(weektab);
+
+        TabHost.TabSpec daytab = spendingtabHost.newTabSpec("Day Tab");
+        daytab.setContent(R.id.day_Tab);
+        daytab.setIndicator("일별");
+        spendingtabHost.addTab(daytab);
 
     }
 }
