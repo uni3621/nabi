4688e58f18319383bb69bd13b397de9bc5aa9918
ggoowlgns <30282681+ggoowlgns@users.noreply.github.com>
2017-08-30 23:34:12 +0900

    a

--- app/src/main/java/com/example/user/myapplication/activity/WeatherActivity.java
+++ app/src/main/java/com/example/user/myapplication/activity/WeatherActivity.java
@@ -3,9 +3,13 @@ package com.example.user.myapplication.activity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -15,27 +19,58 @@ import android.widget.TextView;
 
 import com.example.user.myapplication.R;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.xml.sax.DTDHandler;
+import org.xml.sax.DocumentHandler;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.Parser;
+import org.xml.sax.SAXException;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import android.os.Bundle;
+import android.os.AsyncTask;
 public class WeatherActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    boolean isGPSEnabled = false;
+    boolean isNetworkEnabled = false;
+    boolean isGetLocation = false; // gps상태값
+
+    Location location;
+
+    double lat,lon; //위도,경도
+
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1000; //최소 gps 업데이트 거리 1000미터
+    private static final long MIN_TIME_BW_UPDATES = 1000*6*1;// 최소 업데이트시간 1분
 
 
-    private double lat,lng;
-
-    @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+
+
         final Intent intent = new Intent(this, weathercalender.class);
 
-
+/*
         //현재 위치 (도시명)
+        Geocoder mGeocoder = new Geocoder(this,Locale.KOREA);
+        try {
+            List<Address> addrs = mGeocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            for(Address addr : addrs){
+                int index = addr.getMaxAddressLineIndex();
+                for (int i = 0; i<index ; i++){
 
-
+                }
+        }
+*/
 
         //button 입력
         Button calButton = (Button)findViewById(R.id.calbutton);
@@ -57,7 +92,9 @@ public class WeatherActivity extends AppCompatActivity {
         textTime.setText(currentTime);
 
     }
-
+    public void getWeather(){
+        
+    }
 
 
 }
