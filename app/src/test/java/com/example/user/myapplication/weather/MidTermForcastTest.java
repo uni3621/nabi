2dec594add82901e515282f44ad6406d9a6d6c65
ggoowlgns <30282681+ggoowlgns@users.noreply.github.com>
2017-09-26 13:13:38 +0900

    a

---
+++ app/src/test/java/com/example/user/myapplication/weather/MidTermForcastTest.java
@@ -0,0 +1,20 @@
+package com.example.user.myapplication.weather;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JiHoon on 2017-09-14.
+ */
+public class MidTermForcastTest {
+    @Test
+    public void doInBackground() throws Exception {
+
+    }
+
+    @Test
+    public void onPostExecute() throws Exception {
+
+    }
+}
